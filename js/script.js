3b2ea5e9407239756b3240626ac1b6f0e216fc0c
Andreasgura <andre-56@hotmail.it>
2024-03-15 15:28:50 +0100

    generate square function

--- js/script.js
+++ js/script.js
@@ -11,9 +11,24 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+
+creo un div.grid   .d-flex-justify-coontent-center.align-items-center
+dentro il div inserisco N volte una fz che mi genera un quadrato. ogni quadrato ha all'interno, sempre al centro (quindi aggiungo classi), un numero (o in generale qualcosa di scritto)
+
 */
 
 
-let elBtnPlay = document.getElementById('playbtn')
+// let elBtnPlay = document.getElementById('playbtn')
 
-console.log(elBtnPlay)
+// console.log(elBtnPlay)
+// elBtnPlay.addEventListener('click', function(){
+
+// })
+
+function generateSquares(content) {
+    let newElement = document.createElement(`div`);
+    newElement.classList.add('square') 
+    newElement.innerHTML = `<span> + ${content} + <span>`
+    return newElement
+}
+
